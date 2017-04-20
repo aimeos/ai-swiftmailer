d488acaa247e3c20c8f27fbc2a22b4f2edcc99bf
Aimeos <aimeos@aimeos.org>
2017-04-21 00:24:50 +0200

    Compatibility to PHPUnit 6.x

--- lib/custom/tests/MW/Mail/SwiftTest.php
+++ lib/custom/tests/MW/Mail/SwiftTest.php
@@ -7,7 +7,7 @@ namespace Aimeos\MW\Mail;
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
  * @copyright Aimeos (aimeos.org), 2014-2016
  */
-class SwiftTest extends \PHPUnit_Framework_TestCase
+class SwiftTest extends \PHPUnit\Framework\TestCase
 {
 	private $object;
 	private $mock;
