5ab2cd3e524fd7d163313ba155336b02727b8dbe
Aimeos <aimeos@aimeos.org>
2021-06-18 09:21:27 +0200

    Minor coding style improvement

--- manifest.php
+++ manifest.php
@@ -1,11 +1,11 @@
 <?php
 
-return array(
+return [
 	'name' => 'ai-swiftmailer',
-	'depends' => array(
+	'depends' => [
 		'aimeos-core',
-	),
-	'include' => array(
+	],
+	'include' => [
 		'lib/custom/src',
-	),
-);
+	],
+];
